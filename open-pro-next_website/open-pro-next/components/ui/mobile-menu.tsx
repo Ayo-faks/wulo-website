7758bbf61548ff515a082848de0b7b039c75661c
ayoolafakoya <ayoolafakoya@gmail.com>
2024-10-21 15:40:12 +0100

    fixed waitlist mobile

--- open-pro-next_website/open-pro-next/components/ui/mobile-menu.tsx
+++ open-pro-next_website/open-pro-next/components/ui/mobile-menu.tsx
@@ -135,7 +135,7 @@ export default function MobileMenu() {
                 className="flex rounded-lg px-2 py-1.5 text-white hover:text-indigo-500"
                 onClick={() => setMobileNavOpen(false)}
               >
-                Newsletter
+                Waitlist
               </Link>
             </li>
             <li>
